f62a968b91a63cc02994861991e296d0c750ce68
taryn <taryncharles101@gmail.com>
2024-11-05 11:21:36 +0200

    Connect frontend to Firebase backend for sign-ups

--- script.js
+++ script.js
@@ -7,15 +7,13 @@ document.getElementById("waitingListForm").addEventListener("submit", function(e
     const email = document.querySelector('input[name="email"]').value;
 
     // Send form data to the server
-    fetch('https://pocketproof.app/signup', { // Replace with the live server URL
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name: name, email: email })
-    })
-    .then(response => response.text()) // Assuming server responds with plain text
-    .then(data => {
+fetch('https://signup-m7nt6cwi3q-uc.a.run.app', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email })
+})
+.then(response => response.text()) // Assuming server responds with plain text
+.then(data => {
         // Display success message
         const messageBox = document.getElementById("message");
         messageBox.classList.remove("error");
